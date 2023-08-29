9cd12c8e57cf3368c592b5a8e301db1ee9b4687d
dhan <kedanubalde41@gmail.com>
2023-08-29 14:01:37 +0800

    deploying to railway

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -193,11 +193,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR / 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = BASE_DIR/ 'static/images'
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
